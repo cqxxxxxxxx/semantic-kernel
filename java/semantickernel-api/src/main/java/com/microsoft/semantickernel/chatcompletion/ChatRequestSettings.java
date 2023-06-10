02df6ab8730c7721fefbe4125592f65f9b315911
BG307435 <jxchen35@best-inc.com>
2023-06-10 18:55:53 +0800

    1. ChatRequestSettings fix

--- java/semantickernel-api/src/main/java/com/microsoft/semantickernel/chatcompletion/ChatRequestSettings.java
+++ java/semantickernel-api/src/main/java/com/microsoft/semantickernel/chatcompletion/ChatRequestSettings.java
@@ -1,7 +1,40 @@
 // Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.chatcompletion;
 
+import com.microsoft.semantickernel.semanticfunctions.PromptTemplateConfig;
 import com.microsoft.semantickernel.textcompletion.CompletionRequestSettings;
 
-/** Settings for a chat completion request */
-public class ChatRequestSettings extends CompletionRequestSettings {}
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Settings for a chat completion request
+ */
+public class ChatRequestSettings extends CompletionRequestSettings {
+
+  public ChatRequestSettings(
+      double temperature,
+      double topP,
+      double presencePenalty,
+      double frequencyPenalty,
+      int maxTokens,
+      List<String> stopSequences) {
+    super(temperature, topP, presencePenalty, frequencyPenalty, maxTokens, stopSequences);
+  }
+
+  public ChatRequestSettings() {
+    super();
+  }
+
+
+  public static ChatRequestSettings fromCompletionConfig(
+      PromptTemplateConfig.CompletionConfig config) {
+    return new ChatRequestSettings(
+        config.getTemperature(),
+        config.getTopP(),
+        config.getPresencePenalty(),
+        config.getFrequencyPenalty(),
+        config.getMaxTokens(),
+        new ArrayList<>());
+  }
+}
